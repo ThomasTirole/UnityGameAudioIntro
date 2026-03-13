f80aafb35a476c8c5e5c0cd287c2c3c04528c63e
Tirtho <thomas.tirole@ik.me>
2026-03-13 16:50:20 +0100

    crickets marche pas

---
+++ Assets/Scripts/Crickets.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Crickets : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public float stopDistance;
+
+    private Transform player;
+    private float defaultVolume;
+
+    void Start()
+    {
+        defaultVolume = audioSource.volume;
+        player = FindFirstObjectByType<PlayerController>().transform;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player == null)
+            return;
+        // distance from us to the player
+        float dist = Vector3.Distance(transform.position, player.position);
+        if(dist > stopDistance)
+            audioSource.volume = defaultVolume;
+        else
+            audioSource.volume = 0.0f;
+    }
+
+}
